a3db61363fdeebee126f567fd7fcf41bbd551cab
Daniel Weinman <namniewd@gmail.com>
2019-05-01 17:04:51 -0400

    hacky awful 1st pass for rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,20 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  result = []
+  options = [['rock'], ['paper'], ['scissors']]
+
+  
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    return options
+  else:
+    for i in range(len(options)):
+      for j in range(len(rock_paper_scissors(n-1))):
+        result += [options[i] + rock_paper_scissors(n-1)[j]]
+    return result
+
 
 
 if __name__ == "__main__":
