c1dc87cb297626b12db0622d5a19f78ef4851fb7
Daniel Weinman <namniewd@gmail.com>
2019-05-01 15:18:08 -0400

    finished first 3 algos

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,22 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n < 1:
+    return 1
+  elif n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = [0 for i in range(n+1)]
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
