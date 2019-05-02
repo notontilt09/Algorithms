38f270496459127960f6b261abf22a9407d6cf6d
Daniel Weinman <namniewd@gmail.com>
2019-05-01 20:44:09 -0400

    finished making_change.py with fast solution

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,9 +2,20 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, denominations, cache=None):
+  if cache == None:
+    cache = [0 for i in range(amount+1)]
+  
+  cache[0] = 1
+  
+  for i in denominations:
+    for higher_amount in range(i, amount+1):
+      diff = higher_amount - i
+      cache[higher_amount] += cache[diff] 
+  
+  return cache[amount]
 
+print(making_change(10000, [1, 5, 10, 25, 50]))
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
