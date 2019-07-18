e59b05267a0a5ec430315578030651f6aba36b3b
Daniel Weinman <namniewd@gmail.com>
2019-07-18 15:24:58 -0400

    cleaned up cookies a bit

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,20 +6,16 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  if n < 1:
+  if not cache:
+    cache = [0 for i in range(n+1)]
+  if n < 0:
+    return 0
+  if n == 0:
     return 1
-  elif n == 1:
-    return 1
-  elif n == 2:
-    return 2
-  elif n == 3:
-    return 4
   elif cache and cache[n] > 0:
     return cache[n]
   else:
-    if not cache:
-      cache = [0 for i in range(n+1)]
-    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 
   return cache[n]
 
